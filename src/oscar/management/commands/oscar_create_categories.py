9110c37eac39198ec2be0b44c1f9ee3c568b779e
CongNT <nguyencong.1210@gmail.com>
2020-07-02 06:11:40 +0000

    Add subcategories

---
+++ src/oscar/management/commands/oscar_create_categories.py
@@ -0,0 +1,27 @@
+import logging
+
+from django.core.management.base import BaseCommand, CommandError
+
+from oscar.core.loading import get_class
+from oscar.apps.catalogue.categories import create_from_breadcrumbs
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        categories = (
+                'English > Grade > 8',
+                'English > Grade > 9',
+                'English > Grade > 10',
+                'English > Grade > 11',
+                'English > Grade > 12',
+                'English > Level > A2',
+                'English > Level > A1',
+                'English > Level > B2',
+                'English > Level > B1',
+                'English > Level > C2',
+                'English > Level > C1',
+                'English > Certificate > IELTS',
+                'English > Certificate > TOEFL',
+                'English > Certificate > TOEIC'
+                )
+        for breadcrumbs in categories:
+            create_from_breadcrumbs(breadcrumbs)
