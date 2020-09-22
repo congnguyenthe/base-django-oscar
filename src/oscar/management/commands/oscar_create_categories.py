1f67bcddcedafb29137db26846ee91287056b38a
CongNT <nguyencong.1210@gmail.com>
2020-09-22 21:02:01 +0700

    Add breadcrumbs for topics

--- src/oscar/management/commands/oscar_create_categories.py
+++ src/oscar/management/commands/oscar_create_categories.py
@@ -15,15 +15,29 @@ class Command(BaseCommand):
                 'English > Grade > 10',
                 'English > Grade > 11',
                 'English > Grade > 12',
+
                 'English > Level > A2',
                 'English > Level > A1',
                 'English > Level > B2',
                 'English > Level > B1',
                 'English > Level > C2',
                 'English > Level > C1',
+
                 'English > Certificate > IELTS',
                 'English > Certificate > TOEFL',
-                'English > Certificate > TOEIC'
+                'English > Certificate > TOEIC',
+
+                'English > Grammar > Reported Speech',
+                'English > Grammar > Conditional',
+                'English > Grammar > Relative Clauses',
+                'English > Grammar > Adverbs',
+                'English > Grammar > Adjectives',
+                'English > Grammar > Nouns',
+                'English > Grammar > Verbs',
+                'English > Grammar > Passive Voice',
+                'English > Grammar > Tenses',
+                'English > Grammar > Articles',
+                'English > Grammar > Word Order'
                 )
         for breadcrumbs in categories:
             create_from_breadcrumbs(breadcrumbs)
