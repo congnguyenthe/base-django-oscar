0bfef90424187a5c0e46c9b4070e07b2c8947ef0
CongNT <nguyencong.1210@gmail.com>
2020-11-24 04:05:44 +0000

    Support check/uncheck all

--- src/oscar/static/oscar/js/oscar/ui.js
+++ src/oscar/static/oscar/js/oscar/ui.js
@@ -245,6 +245,20 @@ var oscar = (function(o, $) {
                     }
                 });
             });
+
+            $('.checkbox-toggle').click(function () {
+                var clicks = $(this).data('clicks')
+                if (clicks) {
+                  //Uncheck all checkboxes
+                  $('input[type=\'checkbox\']').prop('checked', false)
+                  $('.checkbox-toggle .far.fa-check-square').removeClass('fa-check-square').addClass('fa-square')
+                } else {
+                  //Check all checkboxes
+                  $('input[type=\'checkbox\']').prop('checked', true)
+                  $('.checkbox-toggle .far.fa-square').removeClass('fa-square').addClass('fa-check-square')
+                }
+                $(this).data('clicks', !clicks)
+            })
         }
     };
 
